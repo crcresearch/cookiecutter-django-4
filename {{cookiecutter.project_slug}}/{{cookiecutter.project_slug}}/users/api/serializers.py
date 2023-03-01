1e3b3cd74c0dad2c3dbc928558f4701d9d34c97a
CyprianKyalo <cypkyalo17@gmail.com>
2023-03-01 19:02:35 +0300

    Retrieving User Groups

--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/users/api/serializers.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/users/api/serializers.py
@@ -1,13 +1,20 @@
 from django.contrib.auth import get_user_model
+from django.contrib.auth.models import Group
 from rest_framework import serializers
 
 User = get_user_model()
 
+class GroupSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Group
+        fields = ["name"]
 
 class UserSerializer(serializers.ModelSerializer):
+    groups = GroupSerializer(many=True)
+
     class Meta:
         model = User
-        fields = ["username", "name", "url"]
+        fields = ["username", "name", "url", "groups"]
 
         extra_kwargs = {
             "url": {"view_name": "api:user-detail", "lookup_field": "username"}
